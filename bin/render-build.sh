3376b365007e262c6b04fd71cf9302dfbab9c089
mario-horita <mario.horita@gmail.com>
2026-08-29 15:40:26 -0300

    fix(build): optimize build script with asset precompilation dummy secret

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,7 +3,6 @@
 set -o errexit
 
 bundle install
-bundle exec rails tailwindcss:build
-bundle exec rails assets:precompile
+SECRET_KEY_BASE_DUMMY=1 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
